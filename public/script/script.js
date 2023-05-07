963f9167682c1173fd1875558822fa1211f51bfa
Mohit Uniyal <mohituniyal46@gmail.com>
2023-05-07 18:47:15 +0530

    Home Page made with carousel working perfectly

--- public/script/script.js
+++ public/script/script.js
@@ -4,12 +4,12 @@ const leftArrow=arrows[0];
 const rightArrow=arrows[1];
 
 function slideLeft(event){
-    
+    carousel.scrollLeft+=carousel.clientWidth;
 }
 
 function slideRight(event){
-
+    carousel.scrollLeft-=carousel.clientWidth;
 }
 
-leftArrow.addEventListener('click', slideLeft);
-rightArrow.addEventListener('click', slideRight);
+leftArrow.addEventListener('click', slideRight);
+rightArrow.addEventListener('click', slideLeft);
