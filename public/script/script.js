424b47879346816caea427eefca21b059681253d
Mohit Uniyal <mohituniyal46@gmail.com>
2023-05-07 17:40:44 +0530

    Home Page complete without functioning carousel

---
+++ public/script/script.js
@@ -0,0 +1,15 @@
+const carousel=document.querySelector('.carousel');
+const arrows=document.querySelectorAll('.main-image-box i');
+const leftArrow=arrows[0];
+const rightArrow=arrows[1];
+
+function slideLeft(event){
+    
+}
+
+function slideRight(event){
+
+}
+
+leftArrow.addEventListener('click', slideLeft);
+rightArrow.addEventListener('click', slideRight);
